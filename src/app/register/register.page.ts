2ef2675b878e12ee5934dbe6629c172dee8daee0
AngeloRafaelbr <angeloprofissional@gmail.com>
2024-12-02 19:22:25 -0300

    Refinando login e alterando botão logout

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -13,7 +14,11 @@ export class RegisterPage {
   email: string = '';
   password: string = '';
 
-  constructor(private storageService: Storage, private router: Router) {
+  constructor(
+    private storageService: Storage,
+    private router: Router,
+    private alertController: AlertController // Injeta o AlertController
+  ) {
     this.initStorage();
   }
 
@@ -25,15 +30,33 @@ export class RegisterPage {
 
   // Função de registro
   async onRegister() {
+    if (!this.name || !this.email || !this.password) {
+      // Exibe alerta caso algum campo esteja vazio
+      this.showAlert('Aviso', 'Preencha todos os campos com dados válidos!');
+      return;
+    }
+
     if (this.storage) {
       await this.storage.set('user', {
         name: this.name,
         email: this.email,
         password: this.password,
       });
-      console.log('Usuário registrado com sucesso!');
-      this.router.navigate(['/login']);  // Redireciona para a página de login
+
+      // Exibe alerta de sucesso
+      await this.showAlert('Sucesso', 'Usuário registrado com sucesso!');
+      this.router.navigate(['/login']); // Redireciona para a página de login
     }
+  }
+
+  // Exibe um alerta genérico
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
   // Redirecionar para a tela de login
